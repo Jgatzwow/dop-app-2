19f728241e4975f28cbdb9e404a24797921d7848
MSokol <mihasokol1993@gmail.com>
2022-09-02 19:16:10 +0300

    Added set data button

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,22 @@ function App() {
 
     const [data, setData] = useState<Array<DataType>>([])
     const cleanData = () => {
-      setData([])
+        setData([])
     }
-
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             .then(json => setData(json))
     }, [])
-
+    const getData = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setData(json))
+    }
     return (
         <div className="App">
             <Button name={'Clean Data'} callback={cleanData}/>
+            <Button name={'Set Data'} callback={getData}/>
             <ul>
                 {data.map(d => {
                     return <li key={d.id}>
